7b1be9ebee59745bfc19a4f85059b87e8f180f9b
Keith Austin <51486106+Keiaus@users.noreply.github.com>
2023-02-12 11:13:20 -0500

    Update clientServer.py

--- client/clientServer.py
+++ client/clientServer.py
@@ -2,8 +2,8 @@ import pyodbc
 
 server = 'database-1.ci7iawyx7c5x.us-east-1.rds.amazonaws.com,1433'
 database = 'VetAppointmentSystem'
-username = 'Keith'
-password = '123'
+username = ''
+password = ''
 
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=yes;UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
