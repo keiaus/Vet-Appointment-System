812444787932e0577300ab0f3e5411f2baa65a11
keiaus <k.aus2029@gmail.com>
2023-03-20 20:45:03 -0400

    changes

--- someFile.py
+++
@@ -1,8 +0,0 @@
-class AddTwoNumbers:
-    def __init__(self) -> None:
-        x = int(input("Enter a number: "))
-        y = int(input("Enter another number: "))
-
-        total = x + y
-
-        print(f'{x} + {y} = {total}')
--- someFile2.py
+++
@@ -1,3 +0,0 @@
-from someFile import *
-
-AddTwoNumbers()
