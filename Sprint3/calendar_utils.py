a230e123ebe4cd16d927b4a68bbe823bfeefbae4
keiaus <k.aus2029@gmail.com>
2023-03-23 22:09:45 -0400

    Updated calendar

--- Sprint3/calendar_utils.py
+++ Sprint3/calendar_utils.py
@@ -29,16 +29,13 @@ class CalendarUtils():
         #Calendar
         calendar_display_frame = tk.Frame(self.calendar_frame)
         calendar_display_frame.pack(side = "top", fill = "x")
-
         cal = Calendar(calendar_display_frame, selectmode = 'day', year = 2023, month = 3, day = 14)
         date_label_2 = tk.Label(calendar_display_frame, text = f'Selected Date: {cal.get_date()}', font="Times 30 bold")
-        vet_label = tk.Label(calendar_display_frame, text = f'Selected Vet: {cal.get_date()}', font="Times 30 bold")
         vet_label_2 = tk.Label(calendar_display_frame, text = "Test", font="Times 30 bold") # Displays the vet selected
         # vet_label_2 = tk.Label(calendar_display_frame, text = f'{on_click()}', font="Times 30 bold") # Displays the vet selected
 
         cal.grid(row=3, column = 2)
         date_label_2.grid(row = 0, column = 5)
-        vet_label.grid(row=1, column=5)
         vet_label_2.grid(row=2, column=5)
 
         calendar_display_frame.grid_columnconfigure(0, weight=1)
@@ -50,21 +47,25 @@ class CalendarUtils():
         global cal_view_vet_menu_label
 
         Label(calendar_display_frame, text = "").grid()
-        cal_view_vet_menu_label = Label(calendar_display_frame, text = "View/Delete Veterinarians Menu", font = "times 15")
-        cal_view_vet_menu_label.grid(row=4, column=0)
+        cal_view_vet_menu_label = Label(calendar_display_frame, text = "Select a Vet", font = "times 15 bold")
+        cal_view_vet_menu_label.grid(row=5, column=2)
 
         my_data = cursor.execute(query) # SQLAlchem engine result
         my_list = [r for r, in my_data] # create a list 
 
         sel=tk.StringVar()
         cb1 = ttk.Combobox(calendar_display_frame, values=my_list,width=15,textvariable = sel)
-        cb1.grid(row=5, column=0)
+        cb1.grid(row=6, column=2)
+
+        vet_label = tk.Label(calendar_display_frame, text = f'Selected Vet: {sel.get()}', font="Times 30 bold")
+        vet_label.grid(row=1, column=5)
 
         # Onclick date
         def updateLabel(event):
             date_label_2.config(text = "Selected Date: " + cal.get_date())
+            vet_label.config(text = "Selected Vet: " + sel.get())
             ######Finding position and parsing comma
-            parseOutVetID = vet_info.get()
+            parseOutVetID = sel.get()
             parsedList=parseOutVetID.split(",")
             parsedVetID = parsedList[0]
             ###Finding day of the week
@@ -76,9 +77,9 @@ class CalendarUtils():
 
             #date.replace()
             #dayOfWeek=date.isoweekday()
-         #   dtdate = pickedDate.split("/")
-          # start_date = d(dtdate.index(2),dtdate.index(0),dtdate.index(1))
-           #weekday = start_date.weekday()
+            #dtdate = pickedDate.split("/")
+            #start_date = d(dtdate.index(2),dtdate.index(0),dtdate.index(1))
+            #weekday = start_date.weekday()
             
 
             displayVetID.config(text = "###This needs to be vet ID### " + parsedVetID)
@@ -89,18 +90,10 @@ class CalendarUtils():
         cal.bind("<<CalendarSelected>>", updateLabel)
    
 
-        queryVetSchedule = "SELECT Concat(VetID, ', ', VetFirstName, ', ', VetLastName) FROM VETACCOUNTINFO"
-
-
-
-
-
+        # queryVetSchedule = "SELECT Concat(VetID, ', ', VetFirstName, ', ', VetLastName) FROM VETACCOUNTINFO"
 
         ##date_label = tk.Label(calendar_display_frame, text = "Selected Date: ")
         ##date_label.grid(row=2, column=0)
-
-
-
 
         ##############################
         ##this needs to display VET ID 
@@ -110,7 +103,6 @@ class CalendarUtils():
 
         DisplayDay = tk.Label(calendar_display_frame, text = sel)
         DisplayDay.grid(row=3, column=1)
-        
         
         # def on_click():
         #     selected_vet = Label(calendar_display_frame, text=f'{cb1.get()}')
